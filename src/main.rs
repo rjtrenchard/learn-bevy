1eebdd07d15de7380a527a460e586671fe1d230e
RJ Trenchard <rjtrenchard@gmail.com>
2025-08-02 10:58:46 -0700

    add systems

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_systems(Update, hello_world).run();
+    App::new()
+        .add_systems(Update, hello_world)
+        .add_systems(Update, test)
+        .run();
 }
 
 fn hello_world() {
     println!("hello world");
+    println!("butts");
 }
+
+fn test() {
+    println!("{:?}", AStruct(12.0));
+}
+
+#[derive(Debug)]
+struct AStruct(f32);
