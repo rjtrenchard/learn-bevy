5d336a7348206645c7369583492b210216a0f4d7
RJ Trenchard <rjtrenchard@gmail.com>
2025-08-11 00:08:52 -0700

    add positions to startup schedule

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use bevy::prelude::*;
 
 fn main() {
+    let pos = Position {
+        x: 1.0,
+        y: 2.0,
+        z: 3.0,
+    };
+
     App::new()
+        .add_systems(Startup, add_position)
+        .add_systems(Update, print_position_system)
         .add_systems(Update, hello_world)
         .add_systems(Update, test)
         .run();
@@ -19,3 +27,34 @@ fn test() {
 
 #[derive(Debug)]
 struct AStruct(f32);
+
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+#[derive(Component)]
+struct Person;
+
+fn add_position(mut commands: Commands) {
+    commands.spawn(
+        ((
+            Person,
+            Position {
+                x: 1.0,
+                y: 2.0,
+                z: 3.0,
+            },
+        )),
+    );
+}
+
+fn print_position_system(query: Query<&Position>) {
+    for position in &query {
+        println!("position: {} {} {}", position.x, position.y, position.z);
+    }
+}
+
+struct Entity(u64);
