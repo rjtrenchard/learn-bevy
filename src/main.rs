42e7d12bd2fe5d6f99c8eab57a9ecc2b855b495a
RJ Trenchard <rjtrenchard@gmail.com>
2025-08-17 21:53:13 -0700

    remove hello world stuff

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,8 @@ fn main() {
     App::new()
         .add_systems(Startup, add_position)
         .add_systems(Update, print_position_system)
-        .add_systems(Update, hello_world)
-        .add_systems(Update, test)
         .run();
 }
-
-fn hello_world() {
-    println!("hello world");
-    println!("butts");
-}
-
-fn test() {
-    println!("{:?}", AStruct(12.0));
-    println!("{}", AStruct(1.001).0);
-}
-
-#[derive(Debug)]
-struct AStruct(f32);
 
 #[derive(Component)]
 struct Position {
