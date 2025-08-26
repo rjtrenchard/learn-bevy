b28e164b4a1e510ff2dab2dcc3cad4deb946c412
RJ Trenchard <rjtrenchard@gmail.com>
2025-08-25 19:37:35 -0700

    disable old code, load a sprite into memory and display it

--- src/main.rs
+++ src/main.rs
@@ -3,72 +3,62 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(HelloPlugin)
-        .add_systems(Startup, add_people)
-        .add_systems(
-            Update,
-            (
-                print_position_system,
-                update_position_system,
-                print_position_system,
-            )
-                .chain(),
-        )
+        .add_plugins(SetupPlugin)
         .run();
 }
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((
-        Person,
-        Position {
-            x: 1.0,
-            y: 2.0,
-            z: 3.0,
-        },
-    ));
-    commands.spawn((
-        Person,
-        Position {
-            x: 3.0,
-            y: 5.0,
-            z: 6.0,
-        },
-    ));
-    commands.spawn((
-        Person,
-        Position {
-            x: 4.0,
-            y: 7.0,
-            z: 8.0,
-        },
-    ));
-}
+// fn add_people(mut commands: Commands) {
+//     commands.spawn((
+//         Person,
+//         Position {
+//             x: 1.0,
+//             y: 2.0,
+//             z: 3.0,
+//         },
+//     ));
+//     commands.spawn((
+//         Person,
+//         Position {
+//             x: 3.0,
+//             y: 5.0,
+//             z: 6.0,
+//         },
+//     ));
+//     commands.spawn((
+//         Person,
+//         Position {
+//             x: 4.0,
+//             y: 7.0,
+//             z: 8.0,
+//         },
+//     ));
+// }
 
-fn print_position_system(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Position>) {
-    if !timer.0.tick(time.delta()).just_finished() {
-        return;
-    }
+// fn print_position_system(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Position>) {
+//     if !timer.0.tick(time.delta()).just_finished() {
+//         return;
+//     }
 
-    for position in &query {
-        println!("position: {} {} {}", position.x, position.y, position.z);
-    }
-}
+//     for position in &query {
+//         println!("position: {} {} {}", position.x, position.y, position.z);
+//     }
+// }
 
-fn update_position_system(
-    time: Res<Time>,
-    mut timer: ResMut<GreetTimer>,
-    mut query: Query<&mut Position, With<Person>>,
-) {
-    if !timer.0.tick(time.delta()).just_finished() {
-        return;
-    }
+// fn update_position_system(
+//     time: Res<Time>,
+//     mut timer: ResMut<GreetTimer>,
+//     mut query: Query<&mut Position, With<Person>>,
+// ) {
+//     if !timer.0.tick(time.delta()).just_finished() {
+//         return;
+//     }
 
-    for mut position in &mut query {
-        if position.x == 1.0 {
-            position.x = 2.0;
-        }
-    }
-}
+//     for mut position in &mut query {
+//         if position.x == 1.0 {
+//             position.x = 2.0;
+//         }
+//     }
+// }
 
 #[derive(Component)]
 struct Position {
@@ -88,10 +78,18 @@ struct Name(String);
 
 struct Entity(u64);
 
-pub struct HelloPlugin;
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
 
-impl Plugin for HelloPlugin {
+    commands.spawn(Sprite::from_image(
+        asset_server.load("kenney_prototypeTextures/PNG/Orange/texture_01.png"),
+    ));
+}
+
+pub struct SetupPlugin;
+impl Plugin for SetupPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        // app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, setup);
     }
 }
